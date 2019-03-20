bef4600f618d3b9ac7e5863adec1367638d61181
litaarinda <litaarinda98@gmail.com>
2019-03-20 15:14:33 +0700

    nihRiz

---
+++ elibrary/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+  Route::get('/login', 'AdminAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'AdminAuth\LoginController@login');
+  Route::post('/logout', 'AdminAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'AdminAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'AdminAuth\RegisterController@register');
+
+  Route::post('/password/email', 'AdminAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'AdminAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'pengguna'], function () {
+  Route::get('/login', 'PenggunaAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'PenggunaAuth\LoginController@login');
+  Route::post('/logout', 'PenggunaAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'PenggunaAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'PenggunaAuth\RegisterController@register');
+
+  Route::post('/password/email', 'PenggunaAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'PenggunaAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'PenggunaAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'PenggunaAuth\ResetPasswordController@showResetForm');
+});
